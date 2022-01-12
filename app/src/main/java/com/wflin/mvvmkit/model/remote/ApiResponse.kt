7d0c0ab78b7515835fb4b823ef6bab9c7f8f0fbe
wflin <funcprintf@gmail.com>
2022-01-12 23:25:11 +0800

    add ApiResponse.kt

---
+++ app/src/main/java/com/wflin/mvvmkit/model/remote/ApiResponse.kt
@@ -0,0 +1,14 @@
+package com.wflin.mvvmkit.model.remote
+
+/**
+ * @author: wflin
+ * @data: 2022/1/12
+ * @desc: http请求返回数据基类
+ */
+abstract class ApiResponse<T>(
+    val errorCode: Int?,
+    val errorMsg: String?,
+    val data: T?
+) {
+    abstract fun isSuccess(): Boolean
+}
